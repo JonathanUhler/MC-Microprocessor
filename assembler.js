0aeab19090ead3aded9df41e08a7c02cad6ddaaf
Jonathan Uhler <jonathan@uhler.com>
2020-10-21 17:01:51 -0700

    //	3.0.0	10/21/2020	Changes in this version: //							-Added constants for the input and output directories //							at the top of the program for easy editing //							-Added instructionFields and instructionValidationTable //							hashes for building the intructions //							-Added function writeFileToData, called from within //							replaceStringInData which writes the hexadecimal //							version of the instructions to an output file //							-Added function buildInstruction to take the instruction //							in assembly language, check for a valid opcode and //							arguments, and convernt it to hex for output

--- assembler.js
+++ assembler.js
@@ -5,7 +5,7 @@
 // redstone torches must go in the program memory
 
 "use strict";
-const AssemblerVersion = "2.0.0";
+const AssemblerVersion = "3.0.0";
 
 // Revision History
 //
@@ -33,6 +33,17 @@ const AssemblerVersion = "2.0.0";
 //								-Moved the value of fs.fileReadSync to the data
 //								variable which is used later in the 
 //								replaceStringInData
+//	3.0.0	10/21/2020	Changes in this version:
+//							-Added constants for the input and output directories
+//							at the top of the program for easy editing
+//							-Added instructionFields and instructionValidationTable
+//							hashes for building the intructions
+//							-Added function writeFileToData, called from within
+//							replaceStringInData which writes the hexadecimal
+//							version of the instructions to an output file
+//							-Added function buildInstruction to take the instruction
+//							in assembly language, check for a valid opcode and
+//							arguments, and convernt it to hex for output
 
 // Version information
 this.Version = AssemblerVersion
@@ -60,12 +71,11 @@ AssemblerMessage("Assembler v" + this.Version);
 //
 // None
 //
-
 function AssemblerMessage(msg, ...args) {
 
 	const AssemblerMessageEnable = true;
 
-    let message = "Assembler Message: " + msg;
+    let message = "A-MSG:    " + msg;
     if (args.length > 0) {
       	message += " " + args.join(", ");
     }
@@ -85,8 +95,55 @@ function AssemblerMessage(msg, ...args) {
 let pc = 0;
 // An object used to index the value of the pc using a label
 let labelHash = {};
+// The locations of the input and output text files (edit from here for easy
+// access)
+const inputDirectory = '/Users/jonathan/Documents/VS Code/Assembler/input.asm';
+const outputDirectory = '/Users/jonathan/Documents/VS Code/Assembler/output.bin';
 //
 // end: GLOBAL VARIABLES
+
+
+// ============================================================================
+// Instruction field descriptors
+//
+// Each field is indexed by the name of the field and is a hash that describes
+// the type of field and the allowable values. Keys to that hash are:
+//
+// isOpcode    True if the field is an opcode
+//
+// isLabel     True if the field is a branch label
+//
+// startsAt    Right-most bit of the field. This is also the amount to left
+//             shift a right-justified value to position it to the field.
+//
+// mask        The mask with which to AND a right-justified value to ensure
+//             that it fits in the field
+//
+// pattern     The RegEx pattern to use to match a valid value in that field
+//
+// minVal      The minimum allowable value for the field
+//
+// maxVal      The maximum allowable value for the field
+//
+const instructionFields = {
+	// Opcode: minVal and maxVal aren't used.
+	"opcode"  : {isOpcode: true,  isLabel: false, isImm: false, startsAt: 20,  mask: 0x1f, pattern : /^(\w+)$/,            minVal: 0, maxVal: 0},
+	// The four register fields. Valid register numbers are 0 to 15
+	"rw"      : {isOpcode: false, isLabel: false, isImm: false, startsAt: 16,  mask: 0xf,  pattern : /^r(\d+)$/,           minVal: 0, maxVal: 15},
+	"ra"      : {isOpcode: false, isLabel: false, isImm: false, startsAt: 12,  mask: 0xf,  pattern : /^r(\d+)$/,           minVal: 0, maxVal: 15},
+	"rb"      : {isOpcode: false, isLabel: false, isImm: false, startsAt:  8,  mask: 0xf,  pattern : /^r(\d+)$/,           minVal: 0, maxVal: 15},
+	// The immediate field. It's an 8-bit field, so the valid numbers are 0 to 255, but the
+	// value can be specified as hex, e.g., 0x... or decimal
+	"imm"     : {isOpcode: false, isLabel: false, isImm: true,  startsAt:  0,  mask: 0xff, pattern: /^(0x[0-9a-f]+|\d+)$/, minVal: 0, maxVal: 255},
+	// Lable for branch instructions. minVal and maxVal aren't used.
+	"label"   : {isOpcode: false, isLabel: true, isImm: false,  startsAt:  0,  mask: 0xff, pattern: /^(\w+)$/,             minVal: 0, maxVal: 0}
+} // end: hash intructionFields
+
+// Instruction Validation table
+const instructionValidationTable = {
+	"ori" : {opcode: 0x8, argCount: 3, args:["rw","ra","imm"]},
+	
+} // end: hash instructionValidationTable
 
 
 // ============================================================================
@@ -135,10 +192,12 @@ function replaceStringInData(directory, search, replace) {
 		fileLine = fileLine.trim();
 
 		// Save each of the appended lines to a new array
-		returnData.push(fileLine);
+		if (fileLine.length !== 0) {
+			returnData.push(fileLine);
+		}
 
 		// Print the line to console
-		AssemblerMessage("Data: \n" + fileLine);
+		AssemblerMessage("Line " + i + " Data: \n    " + fileLine);
 	}
 
 	// Return the edited array of lines
@@ -149,101 +208,285 @@ function replaceStringInData(directory, search, replace) {
 
 
 // Take the edited lines from replaceStringInData and execute them
-let lines = replaceStringInData('/Users/jonathan/Documents/VS Code/Assembler/input.txt', /\/\/.*$/, "")
+let lines = replaceStringInData(inputDirectory, /\/\/.*$/, "");
 
 for (let i = 0; i < lines.length; i++) {
 
 	// Display and use the contents and length of each line
 	// Message should look like this 'Line: x has length: y, and states: "z"'
 	AssemblerMessage("Line: " + i, "has length: " + lines[i].length, "and states: \"" + lines[i] + "\"");
-	executeLine(lines[i]);
+	// For each line of the assembly code, parse and print it
+	let line = lines[i];
+	let matchStr = lineParse(line);
+	let buildResult = buildInstruction(matchStr);
+  	if (!buildResult.result) {
+    	AssemblerMessage(`Unable to build instruction for ${line}: ${buildResult.message}`)
+	} 
+	else {
+		writeDataToFile(outputDirectory, `0x${buildResult.instruction}` + "\n")
+
+    	AssemblerMessage(`Assembled instruction for ${line} was 0x${buildResult.instruction}, with branch target \"${buildResult.brTarget}\"; immediate was hex: ${buildResult.immWasHex}`)
+  	}
+	// printParsedLine(line, matchStr);
 
 }
 
 
 // ============================================================================
-// executeLine
+// writeDataToFile
 //
-// Takes in the contents of a single line of assembly code from the data parsed
-// in replaceStringInData, checks for its validity, and executes it.
+// Takes a given directory and some data and writes it to the file given.
 //
 // Arguments--
 //
-// lineContents:		The contents of a single line of assembly code
+// directory:		The location of the file to write the data to on the
+//					current machine
+//
+// dataToWrite:		The data to be written to the specified file
 //
 // Returns--
 //
 // None
 //
-function executeLine(lineContents) {
-	if (lineContents.length === 0) { // Skip empty lines
-		return;
+function writeDataToFile(directory, dataToWrite) {
+	var fs = require("fs");
+
+	fs.appendFile(directory, dataToWrite, (err) => {
+		if (err) {
+			throw err;
+		}
+		else {
+			AssemblerMessage("Output file at \"" + directory + "\" written")
+		}
+	});
+}
+
+
+// ============================================================================
+// buildInstruction
+//
+// Take the results of lineParse and build an instruction from the text.
+//
+// Arguments--
+//
+// parsedValue:		The return value from parseLine
+//
+// Returns--
+//
+// A hash with the results of the attempt to build the instruction, with the
+// following keys:
+//
+// result:        (Boolean) The result of the function. If true, the value
+//                of the instruction is in the instruction key. If false, something
+//                went wrong, and the error message is in the message key
+//
+// instruction:   (Integer) The value of the assembled instruction
+//
+// brTarget:      (String) If the instruction is a branch, this is the
+//                symbolic branch target. "" if not a branch
+//
+// immWasHex:     (Boolean) True if the immediate field was a hex value
+//
+// message:       (String) The error message if the assembly failed
+//
+function buildInstruction(parsedValue) {
+	var instruction = 0;
+	var errorMessage = "";
+	var brTarget = "";
+  
+	let iValEntry = instructionValidationTable[parsedValue.opcode];
+	// Get the instruction validation entry based on the opcode. If this comes
+	// back undefined, then it was an invalid instruction
+	if (iValEntry === undefined) {
+		errorMessage = "Opcode not recogignized: " + parsedValue.opcode;
+		return { result: false, instruction: 0, brTarget: "", immWasHex: false, message: errorMessage };
 	}
-	else { // Parse and execute the assembly code on valid lines
-
+  
+	// Start building the instruction with the opcode
+	instruction |= iValEntry.opcode << instructionFields["opcode"].startsAt;
+  
+	// Confirm that the argument count was what was expected
+	if (iValEntry.argCount !== parsedValue.argCount) {
+		errorMessage = parsedValue.opcode + " had the wrong argument count: expected " + iValEntry.argCount + ", saw " + parsedValue.argCount;
+		return { result: false, instruction: 0, brTarget: "", immWasHex: false, message: errorMessage };
 	}
-
-
-	// // Here if there is a real line to process. A valid line looks like this:
-	// //
-	// // [label:] opcode arg1 [,arg2] [,arg3]
-	// let label = arg1 = arg2 = arg3 = "";
-
-	// // Is there a label?
-	// if (line matches /^([A-Za-z0-1]+)\:\s*(.+)$/) {
-	// // That’s
-	// //	start of line -> ^
-	// //	A label made up of one or more sequences of letters or numbers - and save this -> ([A-Za-z0-1]+)
-	// //	A literal colon -> \:
-	// //	Zero or more whitespace characters -> \s*
-	// //	One or more other characters - and save that -> (.+)
-	// //	The end of line -> $
-	// 	label = match[1];
-			
-	// 	if (labelHash[label] already exists, it’s an error) {
-	// 		labelHash[label] = pc; // Remember that the label refers to the current pc
-	// 		line = match[2]	// Remove the label from the line and remember it
-	// 	}
-	// }
-
-	// // Separate the remaining fields into opcode and arguments
-	// (opcode, arg1, arg2, arg3) = split(line, "\s")
-
-	// // Assume not a directive
-	// let isDirective = false;
-	// // Break out into the different opcodes or directives
-	// switch (opcode) {
-	// 	case ".loc": // .loc <value> sets the pc to this value
-	// 		pc = arg1;
-	// 		isDirective = true;
-	// 		break;
-	// 	case true: // REPLACE THIS LATER: true put as a placeholder to fix syntax errors, replace with: <the rest of the opcodes> 
-	// 		// Process as a function of the opcode. Probably want to call a function to handle the different patterns and letting it check to see
-	// 		// if the opcode has the right type and number of arguments. Then call another function from those to generate the binary for the
-	// 		// instruction and put it into an array indexed by pc. Each of these probably wants to return a tuple with a pretty-printed string, the
-	// 		// hex value of the instruction word and any possible branch target name. Note that this isn’t complete because we can’t resolve the
-	// 		// branches at this point because they may be branching forward and we haven’t yet seen the label. Each of these cases also needs to
-	// 		// increment pc by 1
-	// 		break;
-	// 	default: Error
-	// } // end: switch (opcode)
-} // end: function executeLine
-
-
-
-
-// // Here when done processing the file. Have to clean up the branch targets for any branch instructions
-// function searchTextForInstruction() {
-
-// 	if (instruction.isBranch) {
-// 	    let branchTarget = instruction.branchTarget;
-// 	    if (labelHash[branchTarget] doesn’t exist) {
-// 			AssemblerMessage("ERROR:", labelHash[branchTarget], "doesn't exist!")
-// 		}
-// 	    targetPC = labelHash[branchTarget];
-// 	    // fix up the pretty-print version and the instruction binary to include the branch target PC
-// 	}
+  
+	let immWasHex = false;
+	let fieldArgs = iValEntry.args;
+	for (let i = 0; i < iValEntry.argCount; i++) {
+  
+		// argType is the argument type for this argument from instructionValidationTable[opcode].args[i]
+		let argType = fieldArgs[i];
 	
-// } // end: foreach instruction
+		// iField is the expected argument field
+		let iField = instructionFields[argType];
+	
+		// argSupplied is the text of the argument
+		let argSupplied = parsedValue.args[i];
+	
+		// Use the pattern for this argument type to parse the text value
+		var argMatch = argSupplied.match(iField.pattern);
+	
+		// If the parse fails, it's an error
+		if (argMatch === null || argMatch[1] === undefined) {
+			errorMessage = parsedValue.opcode + " had incorrect argument " + String(i+1) + ": " + argSupplied;
+			return{result: false, instruction: 0, brTarget: "", immWasHex: false, message: errorMessage};
+		}
+	
+		// If this field is a branch target, we're done
+		if (iField.isLabel) {
+			return{result: true, instruction: instruction, immWasHex: false, brTarget: argSupplied, message: ""};
+		}
+	
+		// For anything else, make sure that the value is in range
+		if (argMatch[1] < iField.minVal || argMatch[1] > iField.maxVal) {
+			errorMessage = parsedValue.opcode + " argument " + String(i+1) + " was out of range: " + argSupplied;
+			return{result: false, instruction: 0, brTarget: "", immWasHex: false, message: errorMessage};
+		}
+		// At this point, argMatch[1] is the value of the argument as returned by the RegExp
+		// parse. For registers, which look like "r10", argMatch[1] contains 10. For an
+		// immediate value, it is the value, which may have a 0x prefix.
+	
+		var immValue = 0;
+		// If this is an immediate, we have to separate out the hex and decimal values and
+		// parse them differently. If the parse fails, it's an error
+		if (iField.isImm) {
+			if (argMatch[1].slice(0,2) === "0x") {
+				immValue = parseInt(argMatch[1].slice(2), 16);
+				immWasHex = true;
+			} 
+			else {
+				immValue = parseInt(argMatch[1], 10);
+			}
+			// Did parseInt return an invalid result?
+			if (isNaN(immValue)) {
+				errorMessage = parsedValue.opcode + " argument " + String(i+1) + " had an invalid immediate value: " + argSupplied;
+				return { result: false, instruction: 0, brTarget: "", immWasHex: false, message: errorMessage };
+			}
+			// Jam the value back into argMatch[i] and let the code below handle combining it
+			argMatch[1] = immValue;
+		}
+	
+		// Finally, argMatch[i] has the value of the field. Mask it, shift it to the
+		// right position in the instruction, and OR it in.
+		instruction |= (argMatch[1] & iField.mask) << iField.startsAt;
+  
+	} // end: for (let i = 0; i < iValEntry.argCount; i++)
+  
+	return { result: true, instruction: instruction, immWasHex: immWasHex, brTarget: "", message: "" };
+  
+  
+	// Loop over the arguments and validate them
+  
+} // end: function buildInstruction
 
-// // Dump out the instruction hex with the pretty-printed comment for each instructionxs
+
+// ============================================================================
+// lineParse
+//
+// Parse a line into its constituent parts. Assumes that any comments have
+// been removed and that blank lines (after comment removal) are skipped
+//
+// Arguments--
+//
+// line:      	Line to parse
+//
+// Returns--
+//
+//  Hash returning the information. If this value is undefined, there was a parse
+//  error on the line. If it isn't undefined, it is a hash with the information.
+//  Hash keys are:
+//
+// label:		Any label that was on the line. If this value is "", there was no
+//            	label
+//
+// opcode:    	The opcode from the line (also includes .loc)
+//
+// argCount:  	Count of arguments on the line (0..3)
+//
+// args:      	Array (0..argCount-1) with the argument values
+//
+function lineParse(line) {
+	let label, opcode, match
+	// Trim any leading and trailing whitespace
+	line.trim();
+
+	// Check for a label
+	const labelRegExp = /^(\w+)\:\s*(.+)$/;
+	label = "";
+	match = line.match(labelRegExp);
+	if (match !== null) {
+		label = match[1];
+		line = match[2];
+	}
+
+	// Check for .loc
+	const locRegExp = /^\.loc\s+(\d+)$/;
+	match = line.match(locRegExp);
+	if (match !== null) {
+		opcode = ".loc";
+		line = match[1];
+	} 
+	else {
+		// If not .loc, then it must be an opcode with optional args
+		const opcodeRegExp = /^(\w+)\s*(.*)$/;
+		match = line.match(opcodeRegExp);
+		if (match !== null) {
+				opcode = match[1];
+				line = match[2];
+		}
+		else {
+			// If that doesn't match, then something went wrong
+			AssemblerMessage("ERROR parsing opcode: " + line);
+			return undefined;
+		}
+	}
+
+	// Check for and extract the args
+	line = line.replace(/\s+/g, "");
+	let argCount = 0;
+	let argSplit = [];
+	if (line.length != 0) {
+		argSplit = line.split(",")
+		argCount = argSplit.length;
+		if (argCount > 3) {
+			AssemblerMessage("ERROR: Too many args on line: " + line);
+			return undefined;
+		}
+	}
+
+	// Return the parsed information
+	return {
+		label: label,
+		opcode: opcode,
+		argCount: argCount,
+		args: argSplit,
+	}
+} // end: function lineParse
+
+
+// ============================================================================
+// printParsedLine
+//
+// Print the information returned from parseLine as an example of how to use
+// the information
+//
+// Arguments--
+//
+// line:		the literal line, as passed in to the assembler (written in
+//				assembly code)
+//
+// matchStr:	parsed line to print (this is the hash returned by lineParse)
+//
+function printParsedLine(line, matchStr) {
+	if (matchStr === undefined) {
+	  	AssemblerMessage("Parse error found on line " + line);
+	  	return;
+	}
+	AssemblerMessage(`Line is \"${line}\"`);
+	AssemblerMessage(`  label: \"${matchStr.label}\", opcode: \"${matchStr.opcode}\", argCount: ${matchStr.argCount}`)
+	for (let i = 0; i < matchStr.argCount; i++) {
+	  	AssemblerMessage(`    Argument ${i}: \"${matchStr.args[i]}\"`)
+	}
+} // end: function printParsedLine
+
